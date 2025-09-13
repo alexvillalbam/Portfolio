492bffe31c8a4e5eec0a9cc426dbe61879652aff
alexvillalbam <alexvillalbam@outlook.com>
2025-09-13 19:00:26 -0300

    landing description

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,19 +9,18 @@ const Projects = () => {
 
       <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5'>
         <a
-          href='https://your-project-link.com'
+          href='https://alexvillalbam.github.io/Landing-Page/'
           target='_blank'
           rel='noopener noreferrer'
-          className='block' // so it behaves like a block element
-        >
+          className='block'>
           <div className='text-left space-y-2 border-2 hover:scale-105 transition-all duration-200 cursor-pointer px-8 py-10 border-blue-500 rounded-lg hover:bg-blue-50'>
-            <h1 className='text-3l font-semibold'>Project</h1>
-            <h3 className='text-xl text-gray-700'>Tech Stack</h3>
+            <h1 className='text-3l font-semibold'>Landing Page</h1>
+            <h3 className='text-xl text-gray-700'>React + Typescript</h3>
             <p className='text-xs text-gray-600'>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum
-              placeat nisi qui velit, molestiae quis ut autem aperiam fugiat
-              fuga nobis ea ipsa sit quidem unde laudantium. Officiis, animi
-              ipsum.
+              A responsive landing page built with React and TypeScript. This
+              was my first project using TypeScript, created to practice and
+              strengthen my skills with the language while applying modern
+              front-end development techniques.
             </p>
           </div>
         </a>
