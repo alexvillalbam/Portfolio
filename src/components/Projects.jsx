b227c9476d3753280f2e1e94304319b7af0f79f4
alexvillalbam <alexvillalbam@outlook.com>
2025-09-15 11:10:07 -0300

    proyecto cuestionario albirroja

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -41,15 +41,22 @@ const Projects = () => {
             </p>
           </div>
         </a>
-        <div className='text-left space-y-2 border-2 hover:scale-105 transition-all duration-200 cursor-pointer px-8 py-10 border-blue-500 rounded-lg hover:bg-blue-50'>
-          <h1 className='text-3l font-semibold'>Project</h1>
-          <h3 className='text-xl text-gray-700'>Tech Stack</h3>
-          <p className='text-xs text-gray-600'>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum
-            placeat nisi qui velit, molestiae quis ut autem aperiam fugiat fuga
-            nobis ea ipsa sit quidem unde laudantium. Officiis, animi ipsum.
-          </p>
-        </div>
+        <a
+          href='https://alexvillalbam.github.io/albirroja-quiz/'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='block'>
+          <div className='text-left space-y-2 border-2 hover:scale-105 transition-all duration-200 cursor-pointer px-8 py-10 border-blue-500 rounded-lg hover:bg-blue-50'>
+            <h1 className='text-3l font-semibold'>Project</h1>
+            <h3 className='text-xl text-gray-700'>Tech Stack</h3>
+            <p className='text-xs text-gray-600'>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum
+              placeat nisi qui velit, molestiae quis ut autem aperiam fugiat
+              fuga nobis ea ipsa sit quidem unde laudantium. Officiis, animi
+              ipsum.
+            </p>
+          </div>
+        </a>
       </div>
     </div>
   );
